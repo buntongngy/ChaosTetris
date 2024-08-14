6809efc32d04f1e1c36b2a43be48bcb9d89b3976
buntongngy <144354284+buntongngy@users.noreply.github.com>
2024-08-14 15:17:50 +0700

    Change to new font, add a chaos mod place holder

--- Tetris/Main.cpp
+++ Tetris/Main.cpp
@@ -3,158 +3,182 @@
 #include "color.h"
 #include <iostream>
 
-enum GameState { MENU, GAME, GAME_OVER };
+enum GameState { MENU, GAME, CHAOS_MOD, GAME_OVER };
 
+double chaosTimer = 10;
 double lastUpdateTime = 0;
+GameState gameState;
 
 bool EventTriggered(double interval)
 {
-	double currentTime = GetTime();
-	if (currentTime - lastUpdateTime >= interval)
-	{
-		lastUpdateTime = currentTime;
-		return true;
-	}
-	return false;
+    double currentTime = GetTime();
+    if (currentTime - lastUpdateTime >= interval)
+    {
+        lastUpdateTime = currentTime;
+        return true;
+    }
+    return false;
+}
+
+void DrawGame(Font font, Game& game, bool isChaosMode)
+{
+    DrawTextEx(font, "Score", { 565, 15 }, 38, 2, WHITE);
+    DrawTextEx(font, "Next", { 570, 125 }, 38, 2, WHITE);
+    DrawTextEx(font, "Hold", { 50, 15 }, 38, 2, WHITE);
+
+  
+    DrawRectangleRounded({ 570,55,170, 60 }, 0.3, 6, lightBlue);
+
+    char scoreText[10];
+    sprintf_s(scoreText, "%d", game.score);
+    Vector2 textSize = MeasureTextEx(font, scoreText, 38, 2);
+
+    DrawTextEx(font, scoreText, { 600, 65 }, 38, 2, WHITE);
+
+    DrawRectangleRounded({ 570,175,170, 180 }, 0.3, 6, lightBlue);
+    DrawRectangleRounded({ 15, 65, 170,180 }, 0.3, 6, lightBlue);
+
+    if (game.heldBlock)
+    {
+        switch (game.holdBlock.id)
+        {
+        case 3:
+            game.holdBlock.Draw(-20, 85, false);
+            break;
+        case 4:
+            game.holdBlock.Draw(-20, 65, false);
+            break;
+        case 9:
+            game.holdBlock.Draw(-20, 65, false);
+            break;
+
+        default:
+            game.holdBlock.Draw(-30, 65, false);
+            break;
+        }
+    }
+
+    game.Draw();
+
+    if (isChaosMode)
+    {
+        DrawTextEx(font, "Chaos", { 565, 400 }, 38, 2, WHITE);
+
+        char chaosTimerText[20];
+        sprintf_s(chaosTimerText, "Timer %.1f", chaosTimer);
+        DrawTextEx(font, chaosTimerText, { 565, 450 }, 38, 2, WHITE);
+    }
+
+  
 }
 
 int main() {
 
-	InitWindow(800, 620, "raylib tetis");
-	SetTargetFPS(60);
+    InitWindow(800, 620, "raylib tetis");
+    SetTargetFPS(60);
 
-	Font font = LoadFontEx("Font/TetrisFont.ttf", 64, 0, 0);
+    Font font = LoadFontEx("Font/tetris.ttf", 64, 0, 0);
 
-	Game game = Game();
-	GameState gameState = MENU;
+    Game game = Game();
+    GameState gameState = MENU;
 
-	while (WindowShouldClose() == false) {
+    while (WindowShouldClose() == false) {
 
+        if (gameState == GAME || gameState == CHAOS_MOD)
+        {
+            UpdateMusicStream(game.music);
+            game.HandleInput();
 
+            // Handle the game speed
+            if (EventTriggered(0.2))
+            {
+                game.MoveBlockDown();
+            }
+        }
 
-		if (gameState == GAME)
-		{
-			UpdateMusicStream(game.music);
-			game.HandleInput();
-			//Handle the game speed
-			if (EventTriggered(0.2))
-			{
-				game.MoveBlockDown();
-			}
-		}
+        // Draw Interface
+        BeginDrawing();
+        ClearBackground(darkBlue);
 
+        if (gameState == MENU)
+        {
+            DrawTextEx(font, "Tetris", { 285, 100 }, 64, 2, WHITE);
 
+            Vector2 enterTextPosition = { 225, 300 };
+            Vector2 chaosTextPosition = { 150, 400 };
+            Vector2 escTextPosition = { 250, 500 };
 
+            Vector2 enterTextSize = MeasureTextEx(font, "Press Enter to Start", 32, 2);
+            Vector2 chaosTextSize = MeasureTextEx(font, "Press Shift to enter Chaos Mod", 32, 2);
+            Vector2 escTextSize = MeasureTextEx(font, "Press Esc to Exit", 32, 2);
 
-		//Draw Interface
-		BeginDrawing();
-		ClearBackground(darkBlue);
+            DrawRectangleRounded({ enterTextPosition.x - 10, enterTextPosition.y - 10, enterTextSize.x + 20, enterTextSize.y + 20 }, 0.3, 6, lightBlue);
+            DrawRectangleRounded({ chaosTextPosition.x - 10, chaosTextPosition.y - 10, chaosTextSize.x + 20, chaosTextSize.y + 20 }, 0.3, 6, lightBlue);
+            DrawRectangleRounded({ escTextPosition.x - 10, escTextPosition.y - 10, escTextSize.x + 20, escTextSize.y + 20 }, 0.3, 6, lightBlue);
 
-		if (gameState == MENU)
-		{
-			DrawTextEx(font, "Tetris", { 285, 100 }, 64, 2, WHITE);
+            DrawTextEx(font, "Press Enter to Start", enterTextPosition, 32, 2, WHITE);
+            DrawTextEx(font, "Press Shift to enter Chaos Mod", chaosTextPosition, 32, 2, WHITE);
+            DrawTextEx(font, "Press Esc to Exit", escTextPosition, 32, 2, WHITE);
+        }
+        else if (gameState == GAME)
+        {
+            DrawGame(font, game, false);
+            if (game.gameOver)
+            {
+                gameState = GAME_OVER;
+            }
 
-			Vector2 enterTextPosition = { 225, 300 };
-			Vector2 escTextPosition = { 250, 400 };
+        }
+        else if (gameState == CHAOS_MOD)
+        {
+            DrawGame(font, game, true);
+            if (game.gameOver)
+            {
+                gameState = GAME_OVER;
+            }
 
-			Vector2 enterTextSize = MeasureTextEx(font, "Press Enter to Start", 32, 2);
-			Vector2 escTextSize = MeasureTextEx(font, "Press Esc to Exit", 32, 2);
+        }
+        else if (gameState == GAME_OVER)
+        {
+            char scoreText[50];
+            sprintf_s(scoreText, "Score   %d", game.score);
 
-			
-			DrawRectangleRounded({ enterTextPosition.x - 10, enterTextPosition.y - 10, enterTextSize.x + 20, enterTextSize.y + 20 }, 0.3, 6, lightBlue);
-			DrawRectangleRounded({ escTextPosition.x - 10, escTextPosition.y - 10, escTextSize.x + 20, escTextSize.y + 20 }, 0.3, 6, lightBlue);
+            DrawTextEx(font, "Game Over", { 250, 200 }, 64, 2, WHITE);
+            DrawTextEx(font, scoreText, { 265, 275 }, 38, 2, WHITE);
+            DrawTextEx(font, "Press Enter to restart", { 190, 350 }, 38, 2, WHITE);
+            DrawTextEx(font, "Press Esc to Exit", { 250, 400 }, 32, 2, WHITE);
+        }
 
-			
-			DrawTextEx(font, "Press Enter to Start", enterTextPosition, 32, 2, WHITE);
-			DrawTextEx(font, "Press Esc to Exit", escTextPosition, 32, 2, WHITE);
-		}
-		else if (gameState == GAME)
-		{
-			DrawTextEx(font, "Score", { 565, 15 }, 38, 2, WHITE);
-			DrawTextEx(font, "Next", { 570, 125 }, 38, 2, WHITE);
-			DrawTextEx(font, "Hold", { 15, 15 }, 38, 2, WHITE);
-			if (game.gameOver)
-			{
-				gameState = GAME_OVER;
-			}
+        EndDrawing();
 
-			DrawRectangleRounded({ 570,55,170, 60 }, 0.3, 6, lightBlue);
+        if (gameState == MENU)
+        {
+            if (IsKeyPressed(KEY_ENTER)) {
+                gameState = GAME;
+                game.Reset();
+                PlayMusicStream(game.music);
+            }
+            else if (IsKeyPressed(KEY_RIGHT_SHIFT))
+            {
+                gameState = CHAOS_MOD;
+            }
+            else if (IsKeyPressed(KEY_ESCAPE)) {
+                break;
+            }
+        }
+        else if (gameState == GAME_OVER)
+        {
+            if (IsKeyPressed(KEY_ENTER)) {
+                gameState = GAME;
+                game.Reset();
+                PlayMusicStream(game.music);
+            }
+            else if (IsKeyPressed(KEY_ESCAPE)) {
+                break;
+            }
+        }
 
-			char scoreText[10];
-			sprintf_s(scoreText, "%d", game.score);
-			Vector2 textSize = MeasureTextEx(font, scoreText, 38, 2);
+    }
 
-
-			DrawTextEx(font, scoreText, { 600, 65 }, 38, 2, WHITE);
-		
-			DrawRectangleRounded({ 570,175,170, 180 }, 0.3, 6, lightBlue);
-			DrawRectangleRounded({ 15, 65, 170,180 }, 0.3, 6, lightBlue);
-
-			if (game.heldBlock)
-			{
-
-				
-				switch (game.holdBlock.id)
-				{
-				case 3:
-					game.holdBlock.Draw(-20, 85, false);
-					break;
-				case 4:
-					game.holdBlock.Draw(-20, 65, false);
-					break;
-				case 9:
-					game.holdBlock.Draw(-20, 65, false);
-					break;
-				
-				default:
-					game.holdBlock.Draw(-30, 65, false);
-					break;
-				}
-			}
-
-			game.Draw();
-
-		
-		}
-		else if (gameState == GAME_OVER)
-		{
-
-			char scoreText[50];
-			sprintf_s(scoreText, "Score   %d", game.score);
-
-
-			DrawTextEx(font, "Game Over", { 250, 200 }, 64, 2, WHITE);
-			DrawTextEx(font, scoreText, { 265, 275 }, 38, 2, WHITE);
-			DrawTextEx(font, "Press Enter to restart", { 190, 350 }, 38, 2, WHITE);
-			DrawTextEx(font, "Press Esc to Exit", { 250, 400 }, 32, 2, WHITE);
-		}
-		
-		EndDrawing();
-
-		if (gameState == MENU)
-		{
-			if (IsKeyPressed(KEY_ENTER)) {
-				gameState = GAME;
-				game.Reset();
-				PlayMusicStream(game.music);
-			}
-			else if (IsKeyPressed(KEY_ESCAPE)) {
-				break;
-			}
-		}
-		else if (gameState == GAME_OVER)
-		{
-			if (IsKeyPressed(KEY_ENTER)) {
-				gameState = GAME;
-				game.Reset();
-				PlayMusicStream(game.music);
-			}
-			else if (IsKeyPressed(KEY_ESCAPE)) {
-				break;
-			}
-		}
-
-	}
-
-	CloseWindow();
+    CloseWindow();
 }
